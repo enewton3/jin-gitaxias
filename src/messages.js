d47852c996882e891cdac8d2a6fb51ed12d10311
Sam Myers <sammyers.dev@gmail.com>
2022-11-28 01:48:33 -0500

    Make 110% more obnoxious

--- src/messages.js
+++ src/messages.js
@@ -1,6 +1,19 @@
 const { sendBombMsgToDB, bombComboToDB } = require("./firebase-utils");
 const { isFourTwenty, calculateFourTwentyProximity } = require("./time-utils");
 
+const numbersTM = {
+  1: ":one:",
+  2: ":two:",
+  3: ":three:",
+  4: ":four:",
+  5: ":five:",
+  6: ":six:",
+  7: ":seven:",
+  8: ":eight:",
+  9: ":nine:",
+  0: ":zero:",
+};
+
 const handleBombMessage = (msg, timezone) => {
   const dataToSend = {
     discordTimestamp: msg.createdTimestamp,
@@ -22,11 +35,20 @@ const handleBombMessage = (msg, timezone) => {
       timezone
     );
 
+    const obnoxiousRegex = new RegExp(Object.keys(numbersTM).join("|"), "gi");
+
+    const replyMsg = `Oop :open_mouth:\n You were:\n\n  ${diff.toFormat(
+      "h 'hours,\n\n'  m 'minutes,\n\n'  s 'seconds,\n\n  and' S 'milliseconds\n\n'"
+    )}  ${after ? "LATE :cold_face:" : "EARLY :hot_face:"}`;
+
     msg.reply(
-      `Oop, You were \n ${diff.toFormat(
-        "h 'hours,\n' m 'minutes,\n' s 'seconds,\n and' S 'milliseconds\n'"
-      )} ${after ? "LATE :cold_face:" : "EARLY :hot_face:"}`
+      replyMsg.replace(obnoxiousRegex, (matched) => numbersTM[matched])
     );
+
+    // Lol gottem
+    if (msg.author.id === process.env.HUNTER_ID) {
+      msg.pin();
+    }
   }
 };
 
